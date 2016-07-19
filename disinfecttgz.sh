ddfea4266bea559e5a2c333fefd277beb7eec08b
heyned <eduardo.olv.slv@gmail.com>
2016-07-19 13:10:00 -0300

    remove mf.tgz kill all process and leave ready to aircontrol update

--- disinfecttgz.sh
+++ disinfecttgz.sh
@@ -11,16 +11,16 @@ if [ -e "$FILE" ] ; then
     #Remove the virus
     rm mf.tgz
     rm -Rf .mf
-    rm -Rf mcuser
+    #rm -Rf mcuser
     rm rc.preststart
 	rm rc.poststart
     # Preserve ISP custom scripts Colaboration PVi1 (Git user)
-    sed -i '/mf\/mother/d' /etc/persistent/rc.poststart
+    #sed -i '/mf\/mother/d' /etc/persistent/rc.poststart
     rm rc.prestart
     #Remove mcuser in passwd - by Alexandre
-    sed -ir '/mcad/ c ' /etc/inittab
-    sed -ir '/mcuser/ c ' /etc/passwd
-    sed -ir '/mother/ c ' /etc/passwd
+    #sed -ir '/mcad/ c ' /etc/inittab
+    #sed -ir '/mcuser/ c ' /etc/passwd
+    #sed -ir '/mother/ c ' /etc/passwd
     #Change HTTP port for 81 | Need access http://IP:81
     #cat /tmp/system.cfg | grep -v http > /tmp/system2.cfg
     #echo "httpd.https.status=disabled" >> /tmp/system2.cfg
@@ -38,8 +38,8 @@ if [ -e "$FILE" ] ; then
     # Change SSH Port
     #sed 's/port=22/port=22/g' /tmp/system.cfg
     #Write new config
-    cfgmtd -w -p /etc/
-    cfgmtd -f /tmp/system.cfg -w
+    #cfgmtd -w -p /etc/
+    #cfgmtd -f /tmp/system.cfg -w
     #Kill process - by Alexandre
     kill -HUP `/bin/pidof init`
     kill -9 `/bin/pidof mcad`
@@ -52,7 +52,7 @@ if [ -e "$FILE" ] ; then
     kill -9 `/bin/pidof infect`
     kill -9 `/bin/pidof scan`
     echo "Clear Completed :)"
-    reboot
+    #reboot
 else
     echo "Clear :) No actions"
     exit
